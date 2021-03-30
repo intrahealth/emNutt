fdf064e2c7c544e15f888c7a20497abfbb983b91
Ally Shaban <allyshaban5@gmail.com>
2021-03-30 15:56:41 +0300

    adding log rotation

--- server/lib/winston.js
+++ server/lib/winston.js
@@ -1,17 +1,35 @@
 const winston = require('winston');
+const moment = require('moment');
 require('winston-daily-rotate-file');
-
-var transport = new winston.transports.DailyRotateFile({
-  dirname: './logs',
-  filename: 'emNutt-%DATE%.log',
-  datePattern: 'YYYY-MM-DD-HH',
-  zippedArchive: true,
-  maxSize: '10m',
-  maxFiles: '14d',
-});
+const fs = require('fs');
+const logDir = '/var/log';
+const logPrefix = 'emNutt-';
+const maxLogFiles = 10;
+const logDatePattern = 'YYYY-MM-DD-HH';
+let transports = [ new winston.transports.Console() ];
+if ( process.env.NODE_ENV !== "test" ) {
+  let transport = new winston.transports.DailyRotateFile({
+    dirname: logDir,
+    filename: `${logPrefix}%DATE%.log`,
+    datePattern: logDatePattern,
+    zippedArchive: true,
+    maxSize: '10m',
+    maxFiles: '10',
+  });
+  transport.on('rotate', () => {
+    deleteOldLogs();
+  });
+  transport.on('new', () => {
+    deleteOldLogs();
+  });
+  transport.on('archive', () => {
+    deleteOldLogs();
+  });
+  transports.push( transport );
+}
 
 const logger = winston.createLogger({
-  transports: [new winston.transports.Console(), transport],
+  transports: transports,
   format: winston.format.combine(
     winston.format.colorize({
       all: true,
@@ -19,7 +37,49 @@ const logger = winston.createLogger({
     winston.format.timestamp({
       format: 'YYYY-MM-DD HH:mm:ss',
     }),
-    winston.format.simple()
+    winston.format.printf(
+      info => `${info.timestamp} ${info.level}: ${info.message}`
+    )
   ),
 });
+
+function deleteOldLogs() {
+  fs.readdir(logDir, (err, files) => {
+    let logs = files.filter((file) => {
+      return file.startsWith(logPrefix);
+    });
+    if(logs.length > maxLogFiles) {
+      let keep = [];
+      for(let file of logs) {
+        if(keep.length < maxLogFiles) {
+          keep.push(file);
+          continue;
+        }
+        let removeKept = file.replace(logPrefix, '').split('.log')[0];
+        let removeKeptIndex = -1;
+        for(let index in keep) {
+          let keptDate = keep[index].replace(logPrefix, '');
+          keptDate = keep[index].split('.log')[0];
+          if(moment(keptDate, logDatePattern) < moment(removeKept, logDatePattern)) {
+            removeKept = keptDate;
+            removeKeptIndex = index;
+          }
+        }
+        if(removeKeptIndex !== -1) {
+          keep.splice(removeKeptIndex, 1);
+          keep.push(file);
+        }
+      }
+      for(let file of logs) {
+        let exist = keep.find((kp) => {
+          return kp === file;
+        });
+        if(!exist) {
+          fs.unlinkSync(`${logDir}/${file}`);
+        }
+      }
+    }
+  });
+}
+deleteOldLogs();
 module.exports = logger;
